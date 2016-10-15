1d0fa4ab545dcd7f7c11d3fe349f59dfa2991b5b
akashdhruv <akashdhruv@gwmail.gwu.edu>
2016-10-14 22:43:03 -0500

    Organizing Code 2

---
+++ heat_tempSolver.F90
@@ -0,0 +1,503 @@
+subroutine heat_tempSolver(tstep,T,u,v,s,pf,thco,cp)
+
+#include "Solver.h"
+
+      !$ use omp_lib
+      use Grid_data
+      use IncompNS_data
+      use HeatAD_data
+      use Driver_data
+      use MPI_interface, only: MPI_applyBC, MPI_physicalBC_temp, MPI_CollectResiduals
+      use Multiphase_data, only: mph_cp2,mph_thco2,mph_max_s,mph_min_s
+      use IBM_data, only: ibm_cp1,ibm_thco1
+
+      implicit none
+      
+      integer, intent(in) :: tstep
+      real, intent(inout), dimension(:,:) :: T,u,v,s,pf,thco,cp
+      !real, allocatable, dimension(:,:) :: T_old
+      real, dimension(Nxb+2,Nyb+2) :: T_old
+
+      integer :: i,j,ii,jj
+
+      real :: u_plus, u_mins, v_plus, v_mins, u_conv, v_conv
+      real :: Tx_plus, Tx_mins, Ty_plus, Ty_mins
+      real :: Tij, Tipj, Timj, Tijp, Tijm
+      real :: Txx, Tyy, th, dxp, dxm, dyp, dym
+      real :: alphax_plus, alphax_mins, alphay_plus, alphay_mins, alpha_interface
+      real :: E_source
+
+      real :: tol
+
+      real :: T_res1
+      real :: Tsat
+
+      ht_T_res = 0.0
+      T_res1 = 0.0
+
+      tol = 0.01
+
+      !allocate(T_old(Nxb+2,Nyb+2))
+
+      Tsat = 373.13
+
+      T_old = T
+
+#ifdef SINGLEPHASE
+
+#ifdef TEMP_SOLVER_CENTRAL
+
+   T(2:Nxb+1,2:Nyb+1) = T_old(2:Nxb+1,2:Nyb+1) &
+  +((dr_dt*ins_inRe)/(ht_Pr*(gr_dx**2)))*(T_old(3:Nxb+2,2:Nyb+1)+T_old(1:Nxb,2:Nyb+1)-2*T_old(2:Nxb+1,2:Nyb+1))&
+  +((dr_dt*ins_inRe)/(ht_Pr*(gr_dy**2)))*(T_old(2:Nxb+1,3:Nyb+2)+T_old(2:Nxb+1,1:Nyb)-2*T_old(2:Nxb+1,2:Nyb+1))&
+  -((dr_dt*(u(2:Nxb+1,2:Nyb+1) + u(1:Nxb,2:Nyb+1))/2)/(gr_dx+gr_dx))&
+   *(T_old(3:Nxb+2,2:Nyb+1)-T_old(1:Nxb,2:Nyb+1))&
+  -((dr_dt*(v(2:Nxb+1,2:Nyb+1) + v(2:Nxb+1,1:Nyb))/2)/(gr_dy+gr_dx))&
+  *(T_old(2:Nxb+1,3:Nyb+2)-T_old(2:Nxb+1,1:Nyb))
+
+#endif
+
+
+#ifdef TEMP_SOLVER_UPWIND
+
+#ifdef IBM
+
+  !_____________________________CONJUGATE HEAT TRANSFER______________________________!
+
+  !$OMP PARALLEL DEFAULT(NONE) PRIVATE(i,j,u_conv,v_conv,u_plus,u_mins,&
+  !$OMP v_plus,v_mins,Tx_plus,Tx_mins,Ty_plus,Ty_mins,ii,jj,th,Tipj,Timj,Txx,Tyy,Tsat,&
+  !$OMP Tij,Tijp,Tijm,alphax_plus,alphay_plus,alphax_mins,alphay_mins,alpha_interface) &
+  !$OMP NUM_THREADS(NTHREADS) &
+  !$OMP SHARED(T,T_old,dr_dt,gr_dy,gr_dx,ht_Pr,ins_inRe,u,v,dr_tile,s,tol,thco,cp,ht_Nu,ibm_cp1,ibm_thco1)
+
+  Tsat = 400.00
+
+  !$OMP DO COLLAPSE(2) SCHEDULE(STATIC)
+  !do jj=2,Nyb+1,dr_tile
+  !do ii=2,Nxb+1,dr_tile
+  !do j=jj,jj+dr_tile-1
+     !do i=ii,ii+dr_tile-1
+  do j=2,Nyb+1
+     do i=2,Nxb+1
+
+     u_conv = (u(i,j)+u(i-1,j))/2.
+     v_conv = (v(i,j)+v(i,j-1))/2.
+
+     u_plus = max(u_conv, 0.)
+     u_mins = min(u_conv, 0.)
+
+     v_plus = max(v_conv, 0.)
+     v_mins = min(v_conv, 0.)
+
+     Tx_plus = T_old(i+1,j)
+     Tx_mins = T_old(i-1,j)
+
+     Ty_plus = T_old(i,j+1)
+     Ty_mins = T_old(i,j-1)
+
+     Tij = T_old(i,j)
+
+     ! Case 1 !
+     if(s(i,j)*s(i+1,j).le.0.d0) then
+
+       !if(s(i,j) .ge. 0.) then
+       !   Tsat = T_old(i,j)-(ht_Nu*(T_old(i,j)-T_old(i+1,j))*gr_dx)
+       !else
+       !   Tsat = T_old(i+1,j)-(ht_Nu*(T_old(i+1,j)-T_old(i,j))*gr_dx)
+       !end if
+
+       if (abs(s(i,j))/(abs(s(i,j))+abs(s(i+1,j))) .gt. tol) then
+
+       th = abs(s(i,j))/(abs(s(i,j))+abs(s(i+1,j)))
+       Tx_plus = (Tsat-T_old(i,j))/th + Tij
+
+       else
+
+       th = abs(s(i-1,j))/(abs(s(i-1,j))+abs(s(i+1,j)))
+       Tx_plus = (Tsat-T_old(i-1,j))/th + T_old(i-1,j)
+      
+       end if
+     end if
+     ! End of Case 1 !
+
+
+     ! Case 2 !
+     if(s(i,j)*s(i-1,j).le.0.d0) then
+
+       !if(s(i,j) .ge. 0.) then
+       !   Tsat = T_old(i,j)-(ht_Nu*(T_old(i,j)-T_old(i-1,j))*gr_dx)
+       !else
+       !   Tsat = T_old(i-1,j)-(ht_Nu*(T_old(i-1,j)-T_old(i,j))*gr_dx)
+       !end if
+
+       if (abs(s(i,j))/(abs(s(i,j))+abs(s(i-1,j))) .gt. tol) then
+
+       th = abs(s(i,j))/(abs(s(i,j))+abs(s(i-1,j)))
+       Tx_mins = (Tsat-T_old(i,j))/th + Tij
+    
+       else
+
+       th = abs(s(i+1,j))/(abs(s(i+1,j))+abs(s(i-1,j)))
+       Tx_mins = (Tsat-T_old(i+1,j))/th + T_old(i+1,j)
+             
+       end if
+     end if
+     ! End of Case 2 !
+
+
+    ! Case 3 !
+    if(s(i,j)*s(i,j+1).le.0.d0) then
+
+      !if(s(i,j) .ge. 0.) then
+      !    Tsat = T_old(i,j)-(ht_Nu*(T_old(i,j)-T_old(i,j+1))*gr_dy)
+      !else
+      !    Tsat = T_old(i,j+1)-(ht_Nu*(T_old(i,j+1)-T_old(i,j))*gr_dy)
+      !end if
+
+      if (abs(s(i,j))/(abs(s(i,j))+abs(s(i,j+1))) .gt. tol) then
+
+      th = abs(s(i,j))/(abs(s(i,j))+abs(s(i,j+1)))
+      Ty_plus = (Tsat-T_old(i,j))/th + Tij
+
+      else
+
+      th = abs(s(i,j-1))/(abs(s(i,j-1))+abs(s(i,j+1)))
+      Ty_plus = (Tsat-T_old(i,j-1))/th + T_old(i,j-1)
+    
+      end if
+    end if
+    ! End of Case 3 !
+
+    ! Case 4 !
+    if(s(i,j)*s(i,j-1).le.0.d0) then
+
+      !if(s(i,j) .ge. 0.) then
+      !    Tsat = T_old(i,j)-(ht_Nu*(T_old(i,j)-T_old(i,j-1))*gr_dy)
+      !else
+      !    Tsat = T_old(i,j-1)-(ht_Nu*(T_old(i,j-1)-T_old(i,j))*gr_dy)
+      !end if
+
+      if (abs(s(i,j))/(abs(s(i,j))+abs(s(i,j-1))) .gt. tol) then
+
+      th = abs(s(i,j))/(abs(s(i,j))+abs(s(i,j-1)))
+      Ty_mins = (Tsat-T_old(i,j))/th + Tij
+
+      else
+
+      th = abs(s(i,j+1))/(abs(s(i,j+1))+abs(s(i,j-1)))
+      Ty_mins = (Tsat-T_old(i,j+1))/th + T_old(i,j+1)
+
+      end if
+    end if
+    ! End of Case 4 !
+
+    if(s(i,j) .ge. 0.0) then
+    T(i,j) = T_old(i,j)+((dr_dt*ins_inRe*(ibm_thco1/ibm_cp1))/(ht_Pr*gr_dx*gr_dx))*(Tx_plus+Tx_mins-2*Tij)&
+                       +((dr_dt*ins_inRe*(ibm_thco1/ibm_cp1))/(ht_Pr*gr_dy*gr_dy))*(Ty_plus+Ty_mins-2*Tij)&
+                       -((dr_dt))*(u_plus*(Tij-Tx_mins)/gr_dx + u_mins*(Tx_plus-Tij)/gr_dx)&
+                       -((dr_dt))*(v_plus*(Tij-Ty_mins)/gr_dy + v_mins*(Ty_plus-Tij)/gr_dy)
+
+    else
+    T(i,j) = T_old(i,j)+((dr_dt*ins_inRe)/(ht_Pr*gr_dx*gr_dx))*(Tx_plus+Tx_mins-2*Tij)&
+                       +((dr_dt*ins_inRe)/(ht_Pr*gr_dy*gr_dy))*(Ty_plus+Ty_mins-2*Tij)&
+                       -((dr_dt))*(u_plus*(Tij-Tx_mins)/gr_dx + u_mins*(Tx_plus-Tij)/gr_dx)&
+                       -((dr_dt))*(v_plus*(Tij-Ty_mins)/gr_dy + v_mins*(Ty_plus-Tij)/gr_dy)
+
+    end if
+
+    end do
+  end do
+  !end do
+  !end do
+
+  !$OMP END DO
+  !$OMP END PARALLEL
+
+  !___________________________END OF CONJUGATE HEAT TRANSFER_____________________!
+
+#else
+  !$OMP PARALLEL DEFAULT(NONE) PRIVATE(i,j,u_conv,v_conv,u_plus,u_mins,&
+  !$OMP v_plus,v_mins,Tx_plus,Tx_mins,Ty_plus,Ty_mins,ii,jj) NUM_THREADS(NTHREADS) &
+  !$OMP SHARED(T,T_old,dr_dt,gr_dy,gr_dx,ht_Pr,ins_inRe,u,v,dr_tile)
+
+  !$OMP DO COLLAPSE(2) SCHEDULE(STATIC)
+
+  !do jj=2,Nyb+1,dr_tile
+  !do ii=2,Nxb+1,dr_tile
+  !do j=jj,jj+dr_tile-1
+     !do i=ii,ii+dr_tile-1
+  do j=2,Nyb+1
+     do i=2,Nxb+1
+
+     u_conv = (u(i,j)+u(i-1,j))/2.
+     v_conv = (v(i,j)+v(i,j-1))/2.
+
+     u_plus = max(u_conv, 0.)
+     u_mins = min(u_conv, 0.)
+
+     v_plus = max(v_conv, 0.)
+     v_mins = min(v_conv, 0.)
+
+     Tx_plus = (T_old(i+1,j)-T_old(i,j))/gr_dx
+     Tx_mins = (T_old(i,j)-T_old(i-1,j))/gr_dx
+
+     Ty_plus = (T_old(i,j+1)-T_old(i,j))/gr_dy
+     Ty_mins = (T_old(i,j)-T_old(i,j-1))/gr_dy
+
+     T(i,j) = T_old(i,j)+((dr_dt*ins_inRe)/(ht_Pr*gr_dx*gr_dx))*(T_old(i+1,j)+T_old(i-1,j)-2*T_old(i,j))&
+                        +((dr_dt*ins_inRe)/(ht_Pr*gr_dy*gr_dy))*(T_old(i,j+1)+T_old(i,j-1)-2*T_old(i,j))&
+                        -((dr_dt))*(u_plus*Tx_mins + u_mins*Tx_plus)&
+                        -((dr_dt))*(v_plus*Ty_mins + v_mins*Ty_plus)
+
+     end do
+  end do
+  !end do
+  !end do
+
+  !$OMP END DO
+  !$OMP END PARALLEL
+#endif
+#endif
+
+     call MPI_applyBC(T)
+     call MPI_physicalBC_temp(T)
+
+     do i=1,Nxb+2
+          ht_T_res = ht_T_res + sum((T(i,:)-T_old(i,:))**2)
+     enddo
+
+     call MPI_CollectResiduals(ht_T_res,T_res1,SUM_DATA)
+
+     ht_T_res = sqrt(T_res1/((Nxb+2)*(Nyb+2)*(nblockx*nblocky)))
+
+#endif
+
+#ifdef MULTIPHASE
+
+#ifdef TEMP_SOLVER_UPWIND
+
+  !______MULTIPHASE TEMPERATURE SOLVER IS IN DEBUG MODE___________!
+
+  !$OMP PARALLEL DEFAULT(NONE) PRIVATE(i,j,u_conv,v_conv,u_plus,u_mins,&
+  !$OMP v_plus,v_mins,Tx_plus,Tx_mins,Ty_plus,Ty_mins,Tij,th,&
+  !$OMP alphax_plus,alphax_mins,alphay_plus,alphay_mins,alpha_interface,Txx,Tyy) NUM_THREADS(NTHREADS) &
+  !$OMP SHARED(T,T_old,dr_dt,ht_Pr,ins_inRe,u,v,s,gr_dx,gr_dy,tol,Tsat,cp,thco,mph_thco2,mph_cp2)
+
+  !$OMP DO COLLAPSE(2) SCHEDULE(STATIC)
+   do j=2,Nxb+1
+     do i=2,Nyb+1
+
+     u_conv = (u(i,j)+u(i-1,j))/2.
+     v_conv = (v(i,j)+v(i,j-1))/2.
+
+     u_plus = max(u_conv, 0.)
+     u_mins = min(u_conv, 0.)
+
+     v_plus = max(v_conv, 0.)
+     v_mins = min(v_conv, 0.)
+
+     Tx_plus = T_old(i+1,j)
+     Tx_mins = T_old(i-1,j)
+
+     Ty_plus = T_old(i,j+1)
+     Ty_mins = T_old(i,j-1)
+
+
+     !Tipj = T_old(i+1,j)
+     !Timj = T_old(i-1,j)
+
+     !Tijp = T_old(i,j+1)
+     !Tijm = T_old(i,j-1)
+
+     Tij = T_old(i,j)
+
+     ! Note - Have to Pair mutually exclusive cases together
+
+     ! Case 1 !
+     if(s(i,j)*s(i+1,j).le.0.d0) then
+
+       if (abs(s(i,j))/(abs(s(i,j))+abs(s(i+1,j))) .gt. tol) then
+
+       th = abs(s(i,j))/(abs(s(i,j))+abs(s(i+1,j)))
+       Tx_plus = (Tsat-T_old(i,j))/th + Tij
+
+       else
+
+       th = abs(s(i-1,j))/(abs(s(i-1,j))+abs(s(i+1,j)))
+       Tx_plus = (Tsat-T_old(i-1,j))/th + T_old(i-1,j)
+       
+       end if
+     end if
+     ! End of Case 1 !
+
+
+     ! Case 2 !
+     if(s(i,j)*s(i-1,j).le.0.d0) then
+
+       if (abs(s(i,j))/(abs(s(i,j))+abs(s(i-1,j))) .gt. tol) then
+
+       th = abs(s(i,j))/(abs(s(i,j))+abs(s(i-1,j)))
+       Tx_mins = (Tsat-T_old(i,j))/th + Tij
+
+       else
+
+       th = abs(s(i+1,j))/(abs(s(i+1,j))+abs(s(i-1,j)))
+       Tx_mins = (Tsat-T_old(i+1,j))/th + T_old(i+1,j)
+       
+       end if
+     end if
+     ! End of Case 2 !
+
+
+    ! Case 3 !
+    if(s(i,j)*s(i,j+1).le.0.d0) then
+
+      if (abs(s(i,j))/(abs(s(i,j))+abs(s(i,j+1))) .gt. tol) then
+
+      th = abs(s(i,j))/(abs(s(i,j))+abs(s(i,j+1)))
+      Ty_plus = (Tsat-T_old(i,j))/th + Tij
+
+      else
+
+      th = abs(s(i,j-1))/(abs(s(i,j-1))+abs(s(i,j+1)))
+      Ty_plus = (Tsat-T_old(i,j-1))/th + T_old(i,j-1)
+     
+      end if
+    end if
+    ! End of Case 3 !
+
+
+    ! Case 4 !
+    if(s(i,j)*s(i,j-1).le.0.d0) then
+
+      if (abs(s(i,j))/(abs(s(i,j))+abs(s(i,j-1))) .gt. tol) then
+
+      th = abs(s(i,j))/(abs(s(i,j))+abs(s(i,j-1)))
+      Ty_mins = (Tsat-T_old(i,j))/th + Tij
+
+      else
+
+      th = abs(s(i,j+1))/(abs(s(i,j+1))+abs(s(i,j-1)))
+      Ty_mins = (Tsat-T_old(i,j+1))/th + T_old(i,j+1)
+      
+      end if
+    end if
+    ! End of Case 4 !
+
+    alphax_plus = ((thco(i,j)/cp(i,j))+(thco(i+1,j)/cp(i+1,j)))*0.5!*(mph_thco2/mph_cp2)
+    alphax_mins = ((thco(i,j)/cp(i,j))+(thco(i-1,j)/cp(i-1,j)))*0.5!*(mph_thco2/mph_cp2)
+    alphay_plus = ((thco(i,j)/cp(i,j))+(thco(i,j+1)/cp(i,j+1)))*0.5!*(mph_thco2/mph_cp2)
+    alphay_mins = ((thco(i,j)/cp(i,j))+(thco(i,j-1)/cp(i,j-1)))*0.5!*(mph_thco2/mph_cp2)
+
+    if (s(i,j)*s(i+1,j) .le. 0.d0) then
+
+        if ( abs(s(i,j))/(abs(s(i,j))+abs(s(i+1,j))) .gt. tol) then
+
+        th = abs(s(i,j))/(abs(s(i,j))+abs(s(i+1,j)))
+        alpha_interface = (thco(i,j)/cp(i,j))*th + &
+                          (1.-th)*(thco(i+1,j)/cp(i+1,j))
+        alphax_plus = (alpha_interface + (thco(i,j)/cp(i,j)))*0.5
+
+        else
+
+        th = abs(s(i-1,j))/(abs(s(i-1,j))+abs(s(i+1,j)))
+        alpha_interface = (thco(i-1,j)/cp(i-1,j))*th + &
+                          (1.-th)*(thco(i+1,j)/cp(i+1,j))
+        alphax_plus = (alpha_interface + (thco(i,j)/cp(i,j)))*0.5
+
+        end if 
+    end if
+
+    if (s(i,j)*s(i-1,j) .le. 0.d0) then
+
+        if ( abs(s(i,j))/(abs(s(i,j))+abs(s(i-1,j))) .gt. tol) then
+
+        th = abs(s(i,j))/(abs(s(i,j))+abs(s(i-1,j)))
+        alpha_interface = (thco(i,j)/cp(i,j))*th + &
+                          (1.-th)*(thco(i-1,j)/cp(i-1,j))
+        alphax_mins = (alpha_interface + (thco(i,j)/cp(i,j)))*0.5
+
+        else
+
+        th = abs(s(i+1,j))/(abs(s(i-1,j))+abs(s(i+1,j)))
+        alpha_interface = (thco(i+1,j)/cp(i+1,j))*th + &
+                          (1.-th)*(thco(i-1,j)/cp(i-1,j))
+        alphax_mins = (alpha_interface + (thco(i,j)/cp(i,j)))*0.5
+
+        end if
+    end if
+
+    if (s(i,j)*s(i,j+1) .le. 0.d0) then
+
+        if ( abs(s(i,j))/(abs(s(i,j))+abs(s(i,j+1))) .gt. tol) then
+
+        th = abs(s(i,j))/(abs(s(i,j))+abs(s(i,j+1)))
+        alpha_interface = (thco(i,j)/cp(i,j))*th + &
+                          (1.-th)*(thco(i,j+1)/cp(i,j+1))
+        alphay_plus = (alpha_interface + (thco(i,j)/cp(i,j)))*0.5
+
+        else
+
+        th = abs(s(i,j-1))/(abs(s(i,j-1))+abs(s(i,j+1)))
+        alpha_interface = (thco(i,j-1)/cp(i,j-1))*th + &
+                          (1.-th)*(thco(i,j+1)/cp(i,j+1))
+        alphay_plus = (alpha_interface + (thco(i,j)/cp(i,j)))*0.5
+
+        end if
+    end if
+
+    if (s(i,j)*s(i,j-1) .le. 0.d0) then
+
+        if ( abs(s(i,j))/(abs(s(i,j))+abs(s(i,j-1))) .gt. tol) then
+
+        th = abs(s(i,j))/(abs(s(i,j))+abs(s(i,j-1)))
+        alpha_interface = (thco(i,j)/cp(i,j))*th + &
+                          (1.-th)*(thco(i,j-1)/cp(i,j-1))
+        alphay_mins = (alpha_interface + (thco(i,j)/cp(i,j)))*0.5
+
+        else
+
+        th = abs(s(i,j+1))/(abs(s(i,j-1))+abs(s(i,j+1)))
+        alpha_interface = (thco(i,j+1)/cp(i,j+1))*th + &
+                          (1.-th)*(thco(i,j-1)/cp(i,j-1))
+        alphay_mins = (alpha_interface + (thco(i,j)/cp(i,j)))*0.5
+
+        end if
+    end if
+
+    alphax_plus = alphax_plus*(mph_thco2/mph_cp2)
+    alphax_mins = alphax_mins*(mph_thco2/mph_cp2)
+    alphay_plus = alphay_plus*(mph_thco2/mph_cp2)  
+    alphay_mins = alphay_mins*(mph_thco2/mph_cp2) 
+
+
+    Txx = (alphax_plus*(Tx_plus-Tij)/gr_dx - alphax_mins*(Tij-Tx_mins)/gr_dx)/gr_dx
+    Tyy = (alphay_plus*(Ty_plus-Tij)/gr_dy - alphay_mins*(Tij-Ty_mins)/gr_dy)/gr_dy
+
+    T(i,j) = T_old(i,j) + dr_dt*((-(u_plus*(Tij-Tx_mins)/gr_dx-u_mins*(Tx_plus-Tij)/gr_dx)&
+                                  -(v_plus*(Tij-Ty_mins)/gr_dy-v_mins*(Ty_plus-Tij)/gr_dy))&
+                                  +(Txx + Tyy))
+
+     end do
+   end do
+  !$OMP END DO
+  !$OMP END PARALLEL
+
+#endif
+
+   do i=1,Nxb+2
+          ht_T_res = ht_T_res + sum((T(i,:)-T_old(i,:))**2)
+   enddo
+
+   call MPI_CollectResiduals(ht_T_res,T_res1,SUM_DATA)
+
+   ht_T_res = sqrt(T_res1/((Nxb+2)*(Nyb+2)*(nblockx*nblocky)))
+
+   call MPI_applyBC(T)
+   call MPI_physicalBC_temp(T)
+
+#endif
+
+  !deallocate(T_old)
+
+end subroutine heat_tempSolver
