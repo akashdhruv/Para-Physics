2851e1e434e26b084b15113e9fe05b9193b4df36
akashdhruv <akashdhruv@gwmail.gwu.edu>
2016-10-22 21:29:04 -0500

    Coupling Energy and NS 2

---
+++ Poisson_solver_VC.F90
@@ -0,0 +1,110 @@
+subroutine Poisson_solver_VC(ps_RHS,ps,ps_rx,ps_ry,ps_res,ps_counter,ps_quant)
+
+  !$ use omp_lib
+  use Grid_data
+  use MPI_data
+  use MPI_interface, ONLY: MPI_applyBC, MPI_CollectResiduals, MPI_physicalBC_pres
+  use Driver_data, ONLY: dr_tile
+
+#include "Solver.h"
+                
+  implicit none
+
+  real, dimension(:,:), intent(in) :: ps_RHS
+
+  real, dimension(:,:), intent(inout) :: ps,ps_rx,ps_ry
+
+  integer, intent(in) :: ps_quant
+
+  !real, allocatable, dimension(:,:) :: ps_old, ps_new
+  real, dimension(Nxb+2,Nyb+2) :: ps_old,ps_new
+
+  real, intent(out) :: ps_res
+
+  real, dimension(:,:), allocatable :: p_priv
+        
+  real :: ps_res1
+
+  integer, intent(out) :: ps_counter
+  integer :: i,j,thread_id,ii,jj
+
+  thread_id = 0
+  ps_old = 0
+  ps_counter = 0
+
+  !allocate(ps_old(Nxb+2,Nyb+2))
+  !allocate(ps_new(Nxb+2,Nyb+2))
+
+  !DIR$ OFFLOAD BEGIN TARGET(mic) in(ps_old,gr_dy,gr_dx,ps_RHS,i,j,thread_id,ps_res1,ps_quant,dr_tile,ii,jj) inout(ps,ps_res,ps_counter)
+
+  !$OMP PARALLEL PRIVATE(i,j,thread_id,ii,jj) DEFAULT(NONE) NUM_THREADS(NTHREADS) &
+  !$OMP SHARED(ps_old,ps_rx,ps_ry,gr_dy,gr_dx,ps_RHS,ps,ps_res,ps_counter,ps_res1,ps_quant,dr_tile)
+
+#if NTHREADS > 1
+  thread_id = OMP_GET_THREAD_NUM()
+#endif
+
+  do while(ps_counter<MaxIt)
+
+     if (thread_id == 0) then
+
+     ps_res = 0  
+     ps_old = ps
+
+     end if
+     
+     !$OMP BARRIER
+
+     !$OMP DO COLLAPSE(2) SCHEDULE(STATIC)
+ 
+     !do jj=2,Nyb+1,dr_tile
+     !do ii=2,Nxb+1,dr_tile
+     !do j=jj,jj+dr_tile-1
+        !do i=ii,ii+dr_tile-1
+     do j=2,Nyb+1
+        do i=2,Nxb+1
+
+           ps(i,j)=((ps_old(i,j+1)/(ps_ry(i,j)*gr_dy*gr_dy))+(ps(i,j-1)/(ps_ry(i,j-1)*gr_dy*gr_dy))&
+                   +(ps_old(i+1,j)/(ps_rx(i,j)*gr_dx*gr_dx))+(ps(i-1,j)/(ps_rx(i-1,j)*gr_dx*gr_dx))&
+                   +ps_RHS(i-1,j-1))&
+                   *(1/((1/(ps_rx(i,j)*gr_dx*gr_dx))+(1/(ps_ry(i,j)*gr_dy*gr_dy))+&
+                        (1/(ps_rx(i-1,j)*gr_dx*gr_dx))+(1/(ps_ry(i,j-1)*gr_dy*gr_dy))))
+
+        end do
+     end do
+     !end do
+     !end do
+
+     !$OMP END DO
+
+     ! Pressure BC
+
+     if (thread_id == 0) then
+
+     call MPI_applyBC(ps)
+
+     if(ps_quant == PRES_VAR) call MPI_physicalBC_pres(ps)
+ 
+     ps_counter = ps_counter + 1
+
+     ps_res = ps_res + sum(sum((ps-ps_old)**2,1))
+
+     call MPI_CollectResiduals(ps_res,ps_res1,SUM_DATA)
+
+     ps_res = sqrt(ps_res1/((Nxb+2)*(Nyb+2)*(nblockx*nblocky)))
+
+     end if
+   
+     !$OMP BARRIER
+
+     if( (ps_res < 0.000001 ) .and. (ps_res .ne. 0) ) exit
+
+  end do
+
+  !$OMP END PARALLEL
+
+  !DIR$ END OFFLOAD
+
+  !deallocate(ps_old,ps_new)
+
+end subroutine Poisson_solver_VC
---
+++ ins_momentum_VD.F90
@@ -0,0 +1,356 @@
+subroutine ins_momentum_VD(tstep,p_counter,p,u,v,visc,rhox,rhoy,s,s2)
+
+       use Poisson_interface, ONLY: Poisson_solver_VC            
+       use Grid_data
+       use Driver_data
+       use MPI_data
+       use IncompNS_data
+       use MPI_interface, ONLY: MPI_applyBC, MPI_CollectResiduals, MPI_physicalBC_vel
+       use IncompNS_interface, ONLY: ins_rescaleVel
+       use IBM_interface, ONLY: IBM_ApplyForcing
+
+#include "Solver.h"
+
+       implicit none
+
+       !-Arguments
+       integer, intent(in) :: tstep
+       integer, intent(out) :: p_counter
+
+       !-Local Variables
+
+       !real, allocatable, dimension(:,:) :: ut,vt,u_old,v_old
+       !real, allocatable, dimension(:,:) :: C1,G1,D1,C2,G2,D2,p_RHS
+
+       real, dimension(Nxb+2,Nyb+2) :: ut,vt,u_old,v_old
+       real, dimension(Nxb,Nyb) :: C1,G1,D1,C2,G2,D2,p_RHS
+       real :: u_res1, v_res1, maxdiv, mindiv
+       integer :: i,j
+       real, intent(inout), dimension(:,:) :: u,v,visc,rhox,rhoy,p,s,s2
+
+       !allocate(C1(Nxb,Nyb))
+       !allocate(G1(Nxb,Nyb))
+       !allocate(D1(Nxb,Nyb))
+
+       !allocate(C2(Nxb,Nyb))
+       !allocate(G2(Nxb,Nyb))
+       !allocate(D2(Nxb,Nyb))
+
+       !allocate(ut(Nxb+2,Nyb+2))
+       !allocate(vt(Nxb+2,Nyb+2))
+
+       !allocate(u_old(Nxb+2,Nyb+2))
+       !allocate(v_old(Nxb+2,Nyb+2))
+
+       !allocate(p_RHS(Nxb,Nyb))
+
+       ins_v_res = 0
+       ins_u_res = 0
+
+       v_res1 = 0
+       u_res1 = 0
+
+       ! Rescaling velocities after first step
+       !if(tstep>1) call ins_rescaleVel(u,v)
+
+       u_old = u
+       v_old = v
+
+       ! Predictor Step
+
+       call Convective_U_VD(u,v,gr_dx,gr_dy,C1)
+       call Diffusive_U_VD(u,visc,rhox,gr_dx,gr_dy,ins_inRe,D1)
+
+       G1 = C1 + D1
+
+       if (tstep == 0) then
+
+              ut(2:Nxb+1,2:Nyb+1)=u(2:Nxb+1,2:Nyb+1)+(dr_dt/1)*(G1)
+              ins_G1_old = G1
+       else
+
+              ut(2:Nxb+1,2:Nyb+1)=u(2:Nxb+1,2:Nyb+1)+(dr_dt/2)*(3*ins_G1_old-G1)
+              ins_G1_old = G1
+       endif
+
+
+       call Convective_V_VD(u,v,gr_dx,gr_dy,C2)
+       call Diffusive_V_VD(v,visc,rhoy,gr_dx,gr_dy,ins_inRe,D2)
+
+       G2 = C2 + D2
+
+       if (tstep == 0) then
+
+              vt(2:Nxb+1,2:Nyb+1)=v(2:Nxb+1,2:Nyb+1)+(dr_dt/1)*(G2)
+              ins_G2_old = G2
+       else
+
+              vt(2:Nxb+1,2:Nyb+1)=v(2:Nxb+1,2:Nyb+1)+(dr_dt/2)*(3*ins_G2_old-G2)
+              ins_G2_old = G2
+       endif
+
+       ! Boundary Conditions
+
+       call MPI_applyBC(ut)
+       call MPI_applyBC(vt)
+       call MPI_physicalBC_vel(ut,vt)
+
+#ifdef IBM
+       ! Immersed Boundary - Predictor BC
+
+       call IBM_ApplyForcing(ut,vt,s,s2)
+
+#endif
+       ! Poisson Solver
+
+       p_RHS = -((1/(gr_dy*dr_dt))*(vt(2:Nxb+1,2:Nyb+1)-vt(2:Nxb+1,1:Nyb)))&
+               -((1/(gr_dx*dr_dt))*(ut(2:Nxb+1,2:Nyb+1)-ut(1:Nxb,2:Nyb+1)))
+
+       call Poisson_solver_VC(p_RHS,p,rhox,rhoy,ins_p_res,p_counter,PRES_VAR)
+
+       ! Corrector Step
+
+       u(2:Nxb+1,2:Nyb+1) = ut(2:Nxb+1,2:Nyb+1) - (dr_dt/gr_dx)*(p(3:Nxb+2,2:Nyb+1)-p(2:Nxb+1,2:Nyb+1))
+       v(2:Nxb+1,2:Nyb+1) = vt(2:Nxb+1,2:Nyb+1) - (dr_dt/gr_dy)*(p(2:Nxb+1,3:Nyb+2)-p(2:Nxb+1,2:Nyb+1))
+
+       ! Boundary Conditions
+
+       call MPI_applyBC(u)
+       call MPI_applyBC(v)
+       call MPI_physicalBC_vel(u,v)
+
+       ! Divergence
+
+       maxdiv = -10.**(10.)
+       mindiv = 10.**(10.)
+
+       maxdiv = max(maxdiv,maxval(((1/(gr_dy))*(v(2:Nxb+1,2:Nyb+1)-v(2:Nxb+1,1:Nyb)))&
+                                  +((1/(gr_dx))*(u(2:Nxb+1,2:Nyb+1)-u(1:Nxb,2:Nyb+1)))))
+
+       mindiv = min(mindiv,minval(((1/(gr_dy))*(v(2:Nxb+1,2:Nyb+1)-v(2:Nxb+1,1:Nyb)))&
+                                  +((1/(gr_dx))*(u(2:Nxb+1,2:Nyb+1)-u(1:Nxb,2:Nyb+1)))))
+
+
+       call MPI_CollectResiduals(maxdiv,ins_maxdiv,MAX_DATA)
+       call MPI_CollectResiduals(mindiv,ins_mindiv,MIN_DATA)
+
+       ! Residuals
+
+       do i=1,Nyb+2
+          ins_u_res = ins_u_res + sum((u(:,i)-u_old(:,i))**2)
+       enddo
+
+       call MPI_CollectResiduals(ins_u_res,u_res1,SUM_DATA)
+       ins_u_res = sqrt(u_res1/((nblockx*nblocky)*(Nxb+2)*(Nyb+2)))
+
+       do i=1,Nyb+1
+          ins_v_res = ins_v_res + sum((v(:,i)-v_old(:,i))**2)
+       enddo
+
+       call MPI_CollectResiduals(ins_v_res,v_res1,SUM_DATA)
+       ins_v_res = sqrt(v_res1/((nblockx*nblocky)*(Nxb+2)*(Nyb+2)))
+
+
+       !deallocate(ut,vt,u_old,v_old)
+       !deallocate(C1,G1,D1,C2,G2,D2,p_RHS)
+
+end subroutine ins_momentum_VD
+
+
+!! CONVECTIVE U !!
+subroutine Convective_U_VD(ut,vt,dx,dy,C1)
+
+#include "Solver.h"
+       
+      implicit none
+
+      real,dimension(Nxb+2,Nyb+2), intent(in) :: ut
+      real,dimension(Nxb+2,Nyb+2), intent(in) :: vt
+
+      real,intent(in) :: dx
+      real,intent(in) :: dy
+
+      !real, allocatable, dimension(:,:) :: ue
+      !real, allocatable, dimension(:,:) :: uw
+      !real, allocatable, dimension(:,:) :: us
+      !real, allocatable, dimension(:,:) :: un
+      !real, allocatable, dimension(:,:) :: vs
+      !real, allocatable, dimension(:,:) :: vn
+
+      real, dimension(Nxb,Nyb) :: ue,uw,us,un,vs,vn
+      real, dimension(Nxb,Nyb), intent(out) :: C1
+
+      !allocate(ue(Nxb,Nyb))
+      !allocate(uw(Nxb,Nyb))
+      !allocate(us(Nxb,Nyb))
+      !allocate(un(Nxb,Nyb))
+      !allocate(vs(Nxb,Nyb))
+      !allocate(vn(Nxb,Nyb))
+
+      ue = (ut(2:Nxb+1,2:Nyb+1)+ut(3:Nxb+2,2:Nyb+1))/2
+      uw = (ut(2:Nxb+1,2:Nyb+1)+ut(1:Nxb,2:Nyb+1))/2
+      us = (ut(2:Nxb+1,2:Nyb+1)+ut(2:Nxb+1,1:Nyb))/2
+      un = (ut(2:Nxb+1,2:Nyb+1)+ut(2:Nxb+1,3:Nyb+2))/2
+      vs = (vt(2:Nxb+1,1:Nyb)+vt(3:Nxb+2,1:Nyb))/2
+      vn = (vt(2:Nxb+1,2:Nyb+1)+vt(3:Nxb+2,2:Nyb+1))/2
+
+      C1 = -((ue**2)-(uw**2))/dx - ((un*vn)-(us*vs))/dy
+
+      !deallocate(ue,uw,us,un,vs,vn)
+
+end subroutine Convective_U_VD
+
+!! CONVECTIVE V !!
+subroutine Convective_V_VD(ut,vt,dx,dy,C2)
+
+#include "Solver.h"
+
+      implicit none
+
+      real,dimension(Nxb+2,Nyb+2), intent(in) :: ut
+      real,dimension(Nxb+2,Nyb+2), intent(in) :: vt
+
+      real, intent(in) :: dx
+      real, intent(in) :: dy      
+
+      !real, allocatable,dimension(:,:) :: vn, vs, ve, vw, ue, uw
+      real,dimension(Nxb,Nyb) :: vn,vs,ve,vw,ue,uw
+      real, dimension(Nxb,Nyb), intent(out) :: C2
+
+      !allocate(vn(Nxb,Nyb))
+      !allocate(vs(Nxb,Nyb))
+      !allocate(ve(Nxb,Nyb))
+      !allocate(vw(Nxb,Nyb))
+      !allocate(ue(Nxb,Nyb))
+      !allocate(uw(Nxb,Nyb))
+
+      vs = (vt(2:Nxb+1,2:Nyb+1)+vt(2:Nxb+1,1:Nyb))/2
+      vn = (vt(2:Nxb+1,2:Nyb+1)+vt(2:Nxb+1,3:Nyb+2))/2
+      ve = (vt(2:Nxb+1,2:Nyb+1)+vt(3:Nxb+2,2:Nyb+1))/2
+      vw = (vt(2:Nxb+1,2:Nyb+1)+vt(1:Nxb,2:Nyb+1))/2
+      ue = (ut(2:Nxb+1,2:Nyb+1)+ut(2:Nxb+1,3:Nyb+2))/2
+      uw = (ut(1:Nxb,2:Nyb+1)+ut(1:Nxb,3:Nyb+2))/2
+
+      C2 = -((ue*ve)-(uw*vw))/dx - ((vn**2)-(vs**2))/dy
+
+      !deallocate(vn,vs,ve,vw,ue,uw)
+
+end subroutine Convective_V_VD
+
+!! DIFFUSIVE U !!
+subroutine Diffusive_U_VD(ut,visc,rho,dx,dy,inRe,D1)
+
+#include "Solver.h"
+
+      implicit none
+
+      real,dimension(Nxb+2,Nyb+2), intent(in) :: ut
+      real,dimension(Nxb+2,Nyb+2), intent(in) :: visc,rho
+      real, intent(in) :: dx
+      real, intent(in) :: dy
+
+      real, intent(in) :: inRe
+
+      !real, allocatable, dimension(:,:) :: uP
+      !real, allocatable, dimension(:,:) :: uN
+      !real, allocatable, dimension(:,:) :: uS
+      !real, allocatable, dimension(:,:) :: uE
+      !real, allocatable, dimension(:,:) :: uW
+
+      real,dimension(Nxb,Nyb) :: uP,uN,uS,uE,uW,txxp,txxm,tyyp,tyym
+      real, dimension(Nxb,Nyb), intent(out) :: D1
+
+      !allocate(uP(Nxb,Nyb))
+      !allocate(uN(Nxb,Nyb))
+      !allocate(uS(Nxb,Nyb))
+      !allocate(uE(Nxb,Nyb))
+      !allocate(uW(Nxb,Nyb))
+
+      uP = ut(2:Nxb+1,2:Nyb+1)
+      uE = ut(3:Nxb+2,2:Nyb+1)
+      uW = ut(1:Nxb,2:Nyb+1)
+      uN = ut(2:Nxb+1,3:Nyb+2)
+      uS = ut(2:Nxb+1,1:Nyb)
+
+      txxp = (inRe*(uE-uP)/dx)*visc(2:Nxb+1,2:Nyb+1)
+      txxm = (inRe*(uP-uW)/dx)*visc(1:Nxb,2:Nyb+1)
+
+      tyyp = inRe*(uN-uP)/dy*0.25*(visc(2:Nxb+1,2:Nyb+1)+visc(1:Nxb,2:Nyb+1)&
+                                  +visc(1:Nxb,3:Nyb+2)+visc(2:Nxb+1,3:Nyb+2))
+
+      tyym = inRe*(uP-uS)/dy*0.25*(visc(2:Nxb+1,2:Nyb+1)+visc(1:Nxb,2:Nyb+1)&
+                                  +visc(1:Nxb,1:Nyb)+visc(2:Nxb+1,1:Nyb))
+
+      !D1 = (inRe/dx)*(((uE-uP)/dx)-((uP-uW)/dx)) + (inRe/dy)*(((uN-uP)/dy)-((uP-uS)/dy))
+
+      !D1 = (inRe/dx)*((uE-uP)/dx)&
+      !    -(inRe/dx)*((uP-uW)/dx)&
+      !    +(inRe/dy)*((uN-uP)/dy)&
+      !    -(inRe/dy)*((uP-uS)/dy)
+
+      D1 = (txxp-txxm)/dx + &
+           (tyyp-tyym)/dy
+
+      D1 = D1/rho(2:Nxb+1,2:Nyb+1)
+
+      !deallocate(uP,uN,uS,uE,uW)
+
+end subroutine Diffusive_U_VD
+
+!! DIFFUSIVE V !!
+subroutine Diffusive_V_VD(vt,visc,rho,dx,dy,inRe,D2)
+
+#include "Solver.h"
+
+      implicit none
+
+      real,dimension(Nxb+2,Nyb+2), intent(in) :: vt
+      real,dimension(Nxb+2,Nyb+2), intent(in) :: visc,rho
+
+      real, intent(in) :: dx
+      real, intent(in) :: dy
+
+      real, intent(in) :: inRe
+
+      !real, allocatable, dimension(:,:) :: vP,vE,vW,vN,vS
+      real,dimension(Nxb,Nyb) :: vP,vE,vW,vN,vS,txxp,txxm,tyyp,tyym
+
+      real, dimension(Nxb,Nyb), intent(out) :: D2
+
+      !allocate(vP(Nxb,Nyb))
+      !allocate(vE(Nxb,Nyb))
+      !allocate(vW(Nxb,Nyb)) 
+      !allocate(vN(Nxb,Nyb))
+      !allocate(vS(Nxb,Nyb))
+
+      vP = vt(2:Nxb+1,2:Nyb+1)
+      vE = vt(3:Nxb+2,2:Nyb+1)
+      vW = vt(1:Nxb,2:Nyb+1)
+      vN = vt(2:Nxb+1,3:Nyb+2)
+      vS = vt(2:Nxb+1,1:Nyb)
+
+      txxp = (inRe*(vE-vP)/dx)*0.25*(visc(2:Nxb+1,2:Nyb+1)+visc(2:Nxb+1,1:Nyb)&
+                                    +visc(3:Nxb+2,2:Nyb+1)+visc(3:Nxb+2,1:Nyb))
+
+      txxm = (inRe*(vP-vW)/dx)*0.25*(visc(2:Nxb+1,2:Nyb+1)+visc(2:Nxb+1,1:Nyb)&
+                                    +visc(1:Nxb,2:Nyb+1)+visc(1:Nxb,1:Nyb))
+
+      tyyp = (inRe*(vN-vP)/dy)*visc(2:Nxb+1,2:Nyb+1)
+      tyym = (inRe*(vP-vS)/dy)*visc(2:Nxb+1,1:Nyb)
+
+      !D2 = (inRe/dx)*(((vE-vP)/dx)-((vP-vW)/dx)) + (inRe/dy)*(((vN-vP)/dy)-((vP-vS)/dy))
+
+      !D2 = (inRe/dx)*((vE-vP)/dx)&
+      !    -(inRe/dx)*((vP-vW)/dx)&
+      !    +(inRe/dy)*((vN-vP)/dy)&
+      !    -(inRe/dy)*((vP-vS)/dy)
+
+      D2 = (txxp-txxm)/dx + &
+           (tyyp-tyym)/dy
+
+      D2 = D2/rho(2:Nxb+1,2:Nyb+1)
+  
+      !deallocate(vP,vE,vW,vN,vS)
+
+end subroutine Diffusive_V_VD
+
