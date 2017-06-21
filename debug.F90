7afacef73dcc94bacc1a4e5840e8498fcf4e1549
akashdhruv <akashdhruv@gwmail.gwu.edu>
2017-06-20 20:46:55 -0400

    debug for SHM

---
+++ debug.F90
@@ -0,0 +1,85 @@
+program debug
+
+
+        use, intrinsic :: ISO_C_BINDING, ONLY: C_PTR, C_F_POINTER
+
+        implicit none
+
+        include "mpif.h"
+
+        integer :: solver_comm, shared_comm, myid, procs
+        integer :: shared_id,shared_procs,ierr
+        integer :: resultlen
+        character*(MPI_MAX_PROCESSOR_NAME) :: nameproc
+        integer(kind=MPI_ADDRESS_KIND) :: sze
+        integer :: disp_unit
+
+        type(C_PTR) :: baseptr
+        integer :: win
+
+        real,pointer,dimension(:,:) :: shared_data
+
+        integer :: Nx,Ny
+
+        Nx = 10
+        Ny = 1
+
+        solver_comm = MPI_COMM_WORLD
+
+        call MPI_INIT(ierr)
+        call MPI_COMM_RANK(solver_comm, myid, ierr)
+        call MPI_COMM_SIZE(solver_comm, procs, ierr)
+
+        call MPI_GET_PROCESSOR_NAME(nameproc,resultlen,ierr)
+
+        call MPI_COMM_SPLIT_TYPE(solver_comm, MPI_COMM_TYPE_SHARED, 0, MPI_INFO_NULL, shared_comm, ierr)
+
+        call MPI_COMM_RANK(shared_comm,shared_id,ierr)
+        call MPI_COMM_SIZE(shared_comm,shared_procs,ierr)
+
+        !print *,"Global rank: ",myid,"Shared rank: ",shared_id,"Proc: ",nameproc(1:resultlen)
+
+        if(shared_id == 0) then
+           
+           sze = Nx*Ny
+
+        else
+
+           sze = 0
+
+        end if
+
+        disp_unit = 1
+
+        call MPI_WIN_ALLOCATE_SHARED(sze,disp_unit,MPI_INFO_NULL,shared_comm,baseptr,win,ierr)
+       
+        if (shared_id /= 0) call MPI_WIN_SHARED_QUERY(win, 0, sze, disp_unit, baseptr)
+        call MPI_BARRIER(shared_comm,ierr)
+
+        call C_F_POINTER(baseptr, shared_data,[Nx,Ny])
+
+        call MPI_WIN_LOCK_ALL(0,win,ierr)
+
+
+        if(shared_id == 1) shared_data(4,1) = 5
+
+        call MPI_BARRIER(shared_comm,ierr)
+
+        if(shared_id == 5) shared_data(4,1) = 2*shared_data(4,1)
+      
+
+        call MPI_WIN_SYNC(win,ierr)
+        call MPI_BARRIER(shared_comm,ierr)
+
+        call MPI_WIN_UNLOCK_ALL(win,ierr)
+
+        print *,"rank = ",myid,"memory = ",shared_data(4,1),sze,disp_unit,baseptr
+
+        call MPI_WIN_FREE(win,ierr)
+
+        call MPI_COMM_FREE(shared_comm,ierr)
+        call MPI_FINALIZE(ierr)
+
+
+
+end program debug
