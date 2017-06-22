2dd2f525c34933d163888c090190c56e66ab5d59
Akash <akashdhruv@gwmail.gwu.edu>
2017-06-22 15:55:22 -0400

    MPI_shared_bc complete

--- src/MPI_applyBC_shared.F90
+++ src/MPI_applyBC_shared.F90
@@ -27,13 +27,13 @@ subroutine MPI_applyBC_shared(local,shared)
 
         end if
 
-        if(shared_part(myid-1+1) == MPI_UNDEFINED .and. x_id>0) &
+        if(shared_part(myid+1-1) == MPI_UNDEFINED .and. x_id>0) &
         call MPI_ISEND(local(2,:), Nyb+2, MPI_REAL, x_id-1, 1, x_comm, send_req,ierr)
 
 
         if(x_id > 0) then
 
-                if(shared_part(myid-1+1) /= MPI_UNDEFINED) then
+                if(shared_part(myid+1-1) /= MPI_UNDEFINED) then
 
                         local(1,:) = shared(Nxb+1,(shared_id-1)*(Nyb+2)+1:(shared_id-1)*(Nyb+2)+(Nyb+2)+1)
 
@@ -48,5 +48,42 @@ subroutine MPI_applyBC_shared(local,shared)
 
         if(shared_part(myid+1+1) == MPI_UNDEFINED .and. x_id < x_procs-1) &
         call MPI_ISEND(local(Nxb+1,:), Nyb+2, MPI_REAL, x_id+1, 2, x_comm, send_req,ierr)
+
+
+        if(y_id < y_procs - 1) then
+
+                if(shared_part(myid+1+x_procs) /= MPI_UNDEFINED) then
+
+                        local(:,Nyb+2) = shared(:,(shared_id+x_procs)*(Nyb+2)+2)
+
+                else
+
+                        call MPI_IRECV(local(:,Nyb+2), Nxb+2, MPI_REAL, y_id+1, 3, y_comm, recv_req, ierr)
+
+
+                end if
+
+        end if
+
+        if(shared_part(myid+1-x_procs) == MPI_UNDEFINED .and. y_id>0) &
+        call MPI_ISEND(local(:,2), Nxb+2, MPI_REAL, y_id-1, 3, y_comm, send_req,ierr)
+
+       if(y_id > 0) then
+
+                if(shared_part(myid+1-x_procs) /= MPI_UNDEFINED) then
+
+                        local(:,1) = shared(:,(shared_id-x_procs)*(Nyb+2)+Nyb+1)
+
+                else
+
+                        call MPI_IRECV(local(:,1), Nxb+2, MPI_REAL, y_id-1, 4, y_comm, recv_req,ierr)
+
+                end if
+
+        end if
+
+        if(shared_part(myid+1+x_procs) == MPI_UNDEFINED .and. y_id < y_procs -1) &
+        call MPI_ISEND(local(:,Nyb+1), Nxb+2, MPI_REAL, y_id+1, 4 ,y_comm, send_req, ierr)
+        
 
 end subroutine MPI_applyBC_shared
