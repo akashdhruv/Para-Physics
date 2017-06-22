ff1955be6289cabe68331883e4c4212a6d788100
Akash <akashdhruv@gwmail.gwu.edu>
2017-06-22 01:23:06 -0400

    splitting x-comm y-comm

--- src/MPI_SHMtest.F90
+++ src/MPI_SHMtest.F90
@@ -14,12 +14,14 @@ program MPI_SHMtest
         integer(kind=MPI_ADDRESS_KIND) :: sze
         integer :: disp_unit
 
+        integer :: x_comm,y_comm,x_id,y_id,x_procs,y_procs,x_grp,y_grp
+
         type(C_PTR) :: baseptr,shareptr
         integer :: win,i,j
 
         real,pointer,dimension(:,:) :: local_data,shared_data
 
-        integer, allocatable,dimension(:) :: world_part, shared_part
+        integer, allocatable,dimension(:) :: world_part,shared_part,x_part,y_part
 
         integer :: Nx,Ny
 
@@ -27,9 +29,14 @@ program MPI_SHMtest
 
         integer :: status(MPI_STATUS_SIZE), send_req, recv_req, world_grp, shared_grp
 
+        integer :: nblockx, nblocky
+
         !_____Defined Block Size________!
         Nx = 2
         Ny = 2
+
+        nblockx = 4
+        nblocky = 4
 
         !_________Define Global Communication Environment___________!
         solver_comm = MPI_COMM_WORLD
@@ -42,6 +49,26 @@ program MPI_SHMtest
         allocate(world_part(procs))
         world_part = (/(I,I=0,procs-1)/)
 
+        !_________Define Communication Based On Grid______________!
+        call MPI_COMM_SPLIT(solver_comm,myid/nblockx,myid,x_comm,ierr)
+        call MPI_COMM_SPLIT(solver_comm,mod(myid,nblockx),myid,y_comm,ierr)
+
+        call MPI_COMM_RANK(x_comm,x_id,ierr)   
+        call MPI_COMM_SIZE(x_comm,x_procs,ierr)
+        call MPI_COMM_GROUP(x_comm, x_grp, ierr)
+
+        call MPI_COMM_RANK(y_comm,y_id,ierr)
+        call MPI_COMM_size(y_comm,y_procs,ierr)
+        call MPI_COMM_GROUP(y_comm, y_grp, ierr)
+
+        allocate(x_part(procs))
+        allocate(y_part(procs))
+
+        do i=1,procs
+                x_part(i) = mod(i-1,nblockx)
+        end do
+        
+        call MPI_BARRIER(solver_comm,ierr)
         !__________Define Shared Communication Environment__________!
 
         call MPI_COMM_SPLIT_TYPE(solver_comm, MPI_COMM_TYPE_SHARED, 0, MPI_INFO_NULL, shared_comm, ierr)
@@ -50,7 +77,8 @@ program MPI_SHMtest
         call MPI_COMM_GROUP(shared_comm, shared_grp,ierr)
 
         allocate(shared_part(procs))
-        call MPI_GROUP_TRANSLATE_RANKS(world_grp,procs,world_part,shared_grp,shared_part,ierr)
+        !call MPI_GROUP_TRANSLATE_RANKS(world_grp,procs,world_part,shared_grp,shared_part,ierr)
+        call MPI_GROUP_TRANSLATE_RANKS(x_grp,procs,x_part,shared_grp,shared_part,ierr)
 
         !________Make on-node processes allocate their chunk of shared Memory____________________!
         sze       = Nx*Ny*sizeof(A)
@@ -76,26 +104,29 @@ program MPI_SHMtest
         call MPI_BARRIER(solver_comm,ierr)
 
         !_____Exchange Information_____!
-        call MPI_applyBC_shared(local_data,shared_data,myid,procs,solver_comm,world_part,&
-                        shared_id,shared_procs,shared_comm,shared_part,Nx,Ny)
+        !call MPI_applyBC_shared(local_data,shared_data,myid,procs,solver_comm,world_part,&
+        !                shared_id,shared_procs,shared_comm,shared_part,Nx,Ny)
        
-        call MPI_BARRIER(solver_comm,ierr)
-        call MPI_BARRIER(shared_comm,ierr)
+        !call MPI_BARRIER(solver_comm,ierr)
+        !call MPI_BARRIER(shared_comm,ierr)
 
         !_______Print Results_____________!
-        print *,"Global Rank: ",myid," Shared Rank: ",shared_id," data(1,1): ",local_data(1,1)," data(2,2): ",local_data(2,2)
+        if(myid == 0) print *,"x_part:",x_part
+        if(myid == 0) print *,"shared_part: ",shared_part
+        
+        !print *,"Global Rank: ",myid," Shared Rank: ",shared_id," data(1,1): ",local_data(1,1)," data(2,2): ",local_data(2,2)
 
-        call MPI_BARRIER(solver_comm, ierr)
-        call MPI_BARRIER(shared_comm, ierr)
+        !call MPI_BARRIER(solver_comm, ierr)
+        !call MPI_BARRIER(shared_comm, ierr)
 
-         if (shared_id == 0) then
-         print *,"Data on rank: ",shared_id
-          do j=1,Ny*shared_procs
-              print *,shared_data(:,j)
-          end do
-         end if
+        ! if (shared_id == 0) then
+        ! print *,"Data on rank: ",shared_id
+        !  do j=1,Ny*shared_procs
+        !      print *,shared_data(:,j)
+        !  end do
+        ! end if
 
-        call MPI_BARRIER(solver_comm,ierr)
+        !call MPI_BARRIER(solver_comm,ierr)
 
         ! if (shared_id == 1) then
 
@@ -108,6 +139,8 @@ program MPI_SHMtest
        !___Deallocate memory and clean up_____!
        deallocate(world_part)
        deallocate(shared_part)
+       deallocate(x_part)
+       deallocate(y_part)
 
        call MPI_BARRIER(shared_comm,ierr)
        call MPI_WIN_FREE(win,ierr)
